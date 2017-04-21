9ed17de8fd26f8ca805cfba8a535f2f094b2b9a2
ashishblr2k <ashishblr2k@gmail.com>
2017-04-21 07:50:49 +0530

    first commit

---
+++ src/main/java/hello/Receiver.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by agup101 on 4/20/2017.
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
